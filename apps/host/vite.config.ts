c948f9aeb14180e0975ea245bf6c0b839c5940a1
Abdul Basit <abasit@moniepoint.com>
2025-01-23 21:31:34 +0500

    let check this one

--- apps/host/vite.config.ts
+++ apps/host/vite.config.ts
@@ -7,7 +7,7 @@ import federation from '@originjs/vite-plugin-federation';
 import path from 'path';
 
 export default defineConfig({
-  base: 'https://cdn-staging.moniepoint.com/account-upgrade-dev/host',
+  base: 'https://d2rpeoqxjornyi.cloudfront.net/host/',
   root: __dirname,
   cacheDir: '../../node_modules/.vite/apps/host',
   server: {
