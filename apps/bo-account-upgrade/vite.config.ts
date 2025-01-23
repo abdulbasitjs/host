2a21c0ad6d3e37bc6ecabf3d1783ea845c3d3eec
Abdul Basit <abasit@moniepoint.com>
2025-01-24 00:35:10 +0500

    tes

--- apps/bo-account-upgrade/vite.config.ts
+++ apps/bo-account-upgrade/vite.config.ts
@@ -7,7 +7,7 @@ import federation from '@originjs/vite-plugin-federation';
 import path from 'path';
 
 export default defineConfig({
-  base: '/bo/',
+  base: './bo/',
   root: __dirname,
   cacheDir: '../../node_modules/.vite/apps/bo',
   server: {
