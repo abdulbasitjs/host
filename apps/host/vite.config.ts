ef65fef78ed1cfdb81a501491b711d26e96c3ba5
Abdul Basit <abasit@moniepoint.com>
2025-01-23 20:11:26 +0500

    reset

--- apps/host/vite.config.ts
+++ apps/host/vite.config.ts
@@ -48,8 +48,8 @@ export default defineConfig({
         './styles': './src/styles.scss',
       },
       remotes: {
-        boApp: 'https://d2rpeoqxjornyi.cloudfront.net/bo/assets/remoteEntry.js',
-        brmApp: 'https://d2rpeoqxjornyi.cloudfront.net/brm/assets/remoteEntry.js',
+        boApp: 'http://localhost:4301/assets/remoteEntry.js',
+        brmApp: 'http://localhost:4302/assets/remoteEntry.js',
       },
       shared: {
         react: {
